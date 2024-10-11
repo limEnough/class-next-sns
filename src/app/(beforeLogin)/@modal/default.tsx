d4d8ec90465e2a9059671d253be992faa18a3d82
limenough <limenough@github.com>
2024-10-11 15:54:16 +0900

    feat: default.tsx 설정

---
+++ src/app/(beforeLogin)/@modal/default.tsx
@@ -0,0 +1,4 @@
+// parallel route 가 필요 없을 때
+export default function Default() {
+  return null;
+}
--- src/app/(beforeLogin)/@modal/i/flow/login/page.tsx
+++ src/app/(beforeLogin)/@modal/i/flow/login/page.tsx
@@ -1,11 +1,11 @@
 /**
- * @라우트 parallel modal
+ * @라우트 parallel route
  * 두 페이지를 나란히, 동시에 보여줄 수 있는 기능
  * 두 page.tsx가 동일한 상위 폴더 내에 있어야 한다.
  */
 "use client";
 
-import style from '@/app/(beforeLogin)/@modal/login.module.css';
+import style from './login.module.css';
 import { useState } from "react";
 
 export default function LoginModal() {
