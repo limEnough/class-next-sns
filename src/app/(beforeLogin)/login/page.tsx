4aef10a9f29d94bf73d26fe544af32a0915a0ae9
limEnough <limEnough@gmail>
2024-10-13 00:00:16 +0900

    refact: page 콘텐츠 컴포넌트화

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -16,10 +16,7 @@
 // 클라이언트에서 리다이렉트 하는 방법
 "use client";
 import { useRouter } from "next/navigation";
-import styles from "@/app/page.module.css";
-import Image from "next/image";
-import Link from "next/link";
-import Logo from "../../../../public/logo.png";
+import Main from "../_component/main";
 
 export default function LoginRoute() {
   const router = useRouter();
@@ -33,22 +30,6 @@ export default function LoginRoute() {
 
   return (
     // 배경을 깔아주기 위해 main 내용 복붙 (children 맞춰줌)
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <Image src={Logo} alt="logo"></Image>
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-
-        <Link href="/i/flow/signup" className={styles.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={styles.login}>
-          로그인
-        </Link>
-      </div>
-    </div>
+    <Main />
   );
 }
