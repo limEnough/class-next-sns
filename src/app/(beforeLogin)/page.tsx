d1e051558374f1f0d59cfe0125195ebb53cef548
limEnough <limEnough@gmail.com>
2024-10-13 00:00:16 +0900

    refact: page 콘텐츠 컴포넌트화

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -5,29 +5,8 @@
  * css module => 간단하게 가자
  * vanila extract => Windows와 문제
  */
-import styles from "@/app/page.module.css";
-import Image from "next/image";
-import Link from "next/link";
-import Logo from "../../../public/logo.png";
+import Main from "./_component/main";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <Image src={Logo} alt="logo"></Image>
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-
-        <Link href="/i/flow/signup" className={styles.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={styles.login}>
-          로그인
-        </Link>
-      </div>
-    </div>
-  );
+  return <Main />;
 }
